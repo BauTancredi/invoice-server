4fef050471f1716677b9a406346d8b4494e32802
BauTancredi <bautistatancredi@hotmail.com>
2022-12-09 03:12:41 +1100

    change status from 400 to 401

--- controllers/userController.js
+++ controllers/userController.js
@@ -80,14 +80,14 @@ exports.login = async (req, res) => {
     });
 
     if (!user) {
-      return res.status(400).json({ msg: "Invalid credentials" });
+      return res.status(401).json({ msg: "Invalid credentials" });
     }
 
     // compare password
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid credentials" });
+      return res.status(401).json({ msg: "Invalid credentials" });
     }
 
     // return jsonwebtoken
