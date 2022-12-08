8d3d2d11488e3fc237d92d35e2c4b6eec59e421b
BauTancredi <bautistatancredi@hotmail.com>
2022-12-08 16:11:06 +1100

    Added routes for authentication

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import prisma from "../prisma";
+
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -26,6 +31,62 @@ app.get("/invoices", async (req, res) => {
     console.log(userOfInvoice1);
 
     res.json(invoices);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong!" });
+  }
+});
+
+// route for signup
+app.post("/signup", async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong!" });
+  }
+});
+
+// route for login
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid credentials!" });
+    }
+
+    // const SECRET = process.env.JWT_SECRET;
+
+    // return jwt token
+    const token = jwt.sign({ email: user.email, id: user }, "SECRET", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ user: { id: user.id, name: user.name }, token });
   } catch (error) {
     res.status(500).json({ error: "Something went wrong!" });
   }
