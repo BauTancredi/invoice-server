0d42f3fb28cdb8130bee3b938dac9722040b8ad4
BauTancredi <bautistatancredi@hotmail.com>
2022-12-08 16:57:23 +1100

    Route for login plus middleware for auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,3 +61,53 @@ exports.signup = async (req, res) => {
     res.status(500).send("Server error");
   }
 };
+
+exports.login = async (req, res) => {
+  // check for errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // check if user exists
+    let user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
+
+    // return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    // console.error(err);
+    res.status(500).send("Server error");
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // route for signup
 router.post(
-  "/",
+  "/signup",
   [
     check("name", "The name is mandatory").not().isEmpty(),
     check("email", "Add a valid email").isEmail(),
@@ -20,37 +20,14 @@ router.post(
 );
 
 // route for login
-// app.post("/login", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const user = await prisma.user.findUnique({
-//       where: {
-//         email,
-//       },
-//     });
-
-//     if (!user) {
-//       return res.status(404).json({ error: "User not found!" });
-//     }
-
-//     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-//     if (!isPasswordCorrect) {
-//       return res.status(400).json({ error: "Invalid credentials!" });
-//     }
-
-//     // const SECRET = process.env.JWT_SECRET;
-
-//     // return jwt token
-//     const token = jwt.sign({ email: user.email, id: user }, "SECRET", {
-//       expiresIn: "1h",
-//     });
-
-//     res.status(200).json({ user: { id: user.id, name: user.name }, token });
-//   } catch (error) {
-//     res.status(500).json({ error: "Something went wrong!" });
-//   }
-// });
+router.post(
+  "/login",
+  check("email", "Add a valid email").isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+  userController.login
+);
 
 module.exports = router;
