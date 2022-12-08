0c78599e3793d7480c0fc4437589e6ce22a51f85
BauTancredi <bautistatancredi@hotmail.com>
2022-12-09 03:12:12 +1100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json({ extended: true }));
 
 const PORT = process.env.PORT || 8000;
